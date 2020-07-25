f7e634e68828b27ff07e6f341b0d87f0e5a327f9
Max Daunarovich <max.daunarovich@gmail.com>
2020-07-26 01:19:32 +0300

    Add code to handle script processing and transaction execution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import * as fcl from "@onflow/fcl";
+import * as sdk from "@onflow/sdk";
+import * as types from "@onflow/types";
+import "./styles.css";
+
+fcl
+  .config()
+  .put("challenge.handshake", "http://localhost:8701/flow/authenticate");
+
+const executeSimpleScript = async (a, b) => {
+  const response = await fcl.send([
+    sdk.script`
+      pub fun main(a: Int, b: Int):Int {
+        return a + b
+      }
+    `,
+    sdk.args([sdk.arg(a, types.Int), sdk.arg(b, types.Int)]),
+  ]);
+
+  return fcl.decode(response);
+};
+
+const simpleTransaction = async () => {
+  const { authorization } = fcl.currentUser();
+  const tx = await fcl.send([
+    fcl.transaction`
+      transaction {
+        prepare(acct: AuthAccount) {
+          log("Transaction Submitted")
+        }
+      }
+    `,
+    sdk.payer(authorization),
+    sdk.proposer(authorization),
+    sdk.authorizations([authorization]),
+    sdk.limit(100),
+  ]);
+
+  console.log({ tx });
+
+  fcl.tx(tx).subscribe((txStatus) => {
+    if (fcl.tx.isExecuted(txStatus)) {
+      console.log("Transaction Executed");
+    }
+  });
+};
+
+const deploy = async () => {
+  // we will deploy our code to account
+}
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleUser = (user) => {
+    if (user.cid) {
+      setUser(user);
+    } else {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    return fcl.currentUser().subscribe(handleUser);
+  }, []);
+
+  const [scriptResult, setScriptResult] = useState(null);
+  const callScript = async () => {
+    const result = await executeSimpleScript(10, 20);
+    setScriptResult(result);
+  };
+
+  const userLoggedIn = user && !!user.cid;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <button onClick={callScript}>Execute Script</button>
+      {scriptResult && (
+        <div>
+          <p className="script-result">Computation Result: {scriptResult}</p>
+        </div>
+      )}
+
+      {!userLoggedIn ? (
+        <button
+          onClick={() => {
+            fcl.authenticate();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Login
+        </button>
+      ) : (
+        <>
+          <h1 className="welcome">Welcome, {user.identity.name}</h1>
+          <p>Your Address</p>
+          <p className="address">{user.addr}</p>
+          <button onClick={simpleTransaction}>Submit Tx</button>
+          <button
+            onClick={() => {
+              fcl.unauthenticate();
+            }}
+          >
+            Logout
+          </button>
+        </>
+      )}
     </div>
   );
 }
